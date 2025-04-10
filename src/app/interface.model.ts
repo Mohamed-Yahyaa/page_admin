e7b783e3a1a76704f72794f8b26d642f9a96916e
Mohamed-Yahyaa <mohamed.yahya.dev@gmail.com>
2025-04-10 17:48:51 +0100

    add logic this dashboard and design

--- src/app/interface.model.ts
+++ src/app/interface.model.ts
@@ -3,10 +3,13 @@
 export interface Admin {
   id: number;
   name: string;
+  utilisateur :string;
   pack: string;
   options: string;
   consommation: string;
+  signature: string;
   sms: string;
   ocr: string;
-  date: string;
+  datecre: string;
+  daterev: string
 }
