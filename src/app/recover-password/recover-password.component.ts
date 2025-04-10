072c488c0517444820b330e58d2f8a43f94617e5
Mohamed-Yahyaa <mohamed.yahya.dev@gmail.com>
2025-04-10 13:19:06 +0100

    recover-password  front end is done

--- src/app/recover-password/recover-password.component.ts
+++ src/app/recover-password/recover-password.component.ts
@@ -1,17 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recover-password',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatCardModule, ReactiveFormsModule],
+  imports: [CommonModule, MatInputModule, MatCardModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './recover-password.component.html',
-  styleUrl: './recover-password.component.css'
+  styleUrls: ['./recover-password.component.css']
 })
 export class RecoverPasswordComponent {
+  loginForm: FormGroup;
+  hidePassword: boolean = true;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Handle form submission logic here
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  navigateToRecoverPassword(): void {
+    this.router.navigate(['/login']); // Adjust the route as needed
+  }
 }
